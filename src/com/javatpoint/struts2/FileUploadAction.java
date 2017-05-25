d7e5d9e86f9b3f794c4ba03a63aa855ae664fc90
Bharath-infospan <bkbharathkumar82@gmail.com>
2017-05-25 16:56:00 -0700

    third commit

--- src/com/javatpoint/struts2/FileUploadAction.java
+++ src/com/javatpoint/struts2/FileUploadAction.java
@@ -9,7 +9,7 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class FileUploadAction extends ActionSupport implements ServletContextAware {
 	private File userImage;
-	private String userImageContentType;
+	private String userImageConten;
 	private String userImageFileName;
 	private ServletContext servletContext;
 	
